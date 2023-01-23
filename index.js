1d4f4072de94fd1b5c110489147173cd3f11f72c
venkata-chandrika <122530302+venkata-chandrika@users.noreply.github.com>
2023-01-23 19:01:51 +0530

    get-days-after-x-days

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var addDays = require("date-fns/addDays");
 let getDaysAfterxDays = (days) => {
   const dateFormat = addDays(new Date(2020, 7, 22), days);
-  return `${datFormat.getDate()}-${
-    datFormat.getMonth() + 1
-  }-${datFormat.getFullYear()}`;
+  return `${dateFormat.getDate()}-${
+    dateFormat.getMonth() + 1
+  }-${dateFormat.getFullYear()}`;
 };
 module.exports = getDaysAfterxDays;
